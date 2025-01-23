104662725410a455700aa221502d2b0540ab7dae
Harshit Sharma <harshitsharma2718@gmail.com>
2025-01-23 18:10:22 +0530

    TrailerContainer

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,26 +1,26 @@
 import React, { useEffect } from 'react'
 import Header from './header'
-import { addNowPlayingMovies } from '../utils/movieSlice';
-import {  useDispatch } from 'react-redux';
-import { API_OPTIONS } from '../utils/constants'
-
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
+import MainContainer from './MainContainer';
+import SecondaryContainer from './SecondaryContainer';
 const Browse = () => {
   // Fetch Data from TMDB API and update stored movies
-  const dispatch = useDispatch();
-  const getNowPlayingMovies = async()=>{
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
-    const json = await data.json();
-    console.log(json.results);
-    dispatch(addNowPlayingMovies(json.results));
-  };
-
-  useEffect(()=>{
-getNowPlayingMovies();
-  },[]);
+  useNowPlayingMovies();
   return (
     <div>
       <Header/>
-     
+      <MainContainer/>
+      <SecondaryContainer/>
+     {
+      /*
+      MainContainer
+      -VideoBackground
+      - VideoTitle
+      SecondaryContainer
+      -MovieList
+      -cards
+      */
+     }
     </div>
   )
 }
